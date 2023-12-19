cdace1fb69a38da7f1732db64100d0e33833b43b
Quentin Guilloteau <guillo0001@dmi-cl-login.dmi.unibas.ch>
2023-12-19 17:00:12 +0100

    eztrace

--- pkgs/eztrace/default.nix
+++ pkgs/eztrace/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitLab, libotf2, cmake, openmpi }:
+{ stdenv, lib, fetchFromGitLab, libotf2, cmake, openmpi, libiberty, opari2, gfortran, libelf }:
 
 stdenv.mkDerivation rec {
   pname = "eztrace";
@@ -16,12 +16,30 @@ stdenv.mkDerivation rec {
   ];
   buildInputs = [
     libotf2 
+    openmpi
+    libiberty
+    opari2
+    gfortran
+    libelf
+  ];
+  cmakeFlags = [
+    "-DEZTRACE_ENABLE_MPI=ON"
+    "-DEZTRACE_ENABLE_CUDA=OFF"
+    "-DEZTRACE_ENABLE_STARPU=OFF"
+    "-DEZTRACE_ENABLE_OPENMP=ON"
+    "-DEZTRACE_ENABLE_POSIXIO=ON"
+    "-DEZTRACE_ENABLE_PTHREAD=ON"
+    "-DEZTRACE_ENABLE_MEMORY=ON"
+    "-DEZTRACE_ENABLE_OMPT=OFF"
+    "-DEZTRACE_ENABLE_PNETCDF=OFF"
+    "-DEZTRACE_ENABLE_NETCDF=OFF"
+    "-DEZTRACE_ENABLE_IOTRACER=OFF"
+    "-DEZTRACE_ENABLE_COMPILER_INSTRUMENTATION=ON"
   ];
 
   meta = {
-    description = "";
-    homepage = "";
+    description = "EZTrace is a tool that automatically generates execution traces from HPC programs.";
+    homepage = "https://gitlab.com/eztrace/eztrace";
     license = lib.licenses.cecill-b;
-    maintainers = with lib.maintainers; [  ];
   };
 }
